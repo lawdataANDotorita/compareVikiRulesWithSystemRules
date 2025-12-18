7a4b77ed093297505155990ac69d35dadc1a388c
lawdataANDotorita <shayz@otorita.net>
2025-12-18 18:24:34 +0200

    improving regex expression aimed at cleaning the system rules from the redundant suffixes

--- compare.py
+++ compare.py
@@ -1,5 +1,6 @@
 from extract_rules import WikiRulesExtractor
 import re
+import os
 import requests
 import pandas as pd
 import Levenshtein
@@ -36,6 +37,7 @@ def main():
 
 
 
+
     # Fetch the content from the URL
     response = requests.get('https://www.lawdata.co.il/lawdata_face_lift_test/getallhoknamesforcompare.asp')
     response.encoding = 'utf-8'  # Ensure proper Hebrew encoding
@@ -45,26 +47,55 @@ def main():
     
     # Clean each item in system_rules to remove non-alphanumeric characters
     cleaned_system_rules = []
-    for text in system_rules:
-        text = re.sub( r'[^a-zA-Zא-ת0-9]*[א-ת"׳]+\s*([-–]\s*)?\d{2,6}[^a-zA-Zא-ת0-9]*$', '', text)
-        text=re.sub(r'\[נוסח [^\]]*\]', '', text)
-        cleaned_system_rules.append(text.strip())
+    rules_variations=[]
 
+    for text in system_rules:
+        textOrg = text
+        text2=text
+
+        text=re.sub(r'[\[(]נוסח [^\])]*[\])]', '', text)
+        text = re.sub( r'[^\(\)[\]\{\}a-zA-Zא-ת0-9]*[א-ת"׳]+\s*([-–]\s*)?\d{2,6}[^\(\)[\]\{\}a-zA-Zא-ת0-9]*$', '', text)
+
+        # text2 = re.sub( r'[^a-zA-Zא-ת0-9]*\s*([-–]\s*)?\d{2,6}[^a-zA-Zא-ת0-9]*$', '', text2)
+        #  text2=re.sub(r'[\[(]נוסח [^\])]*[\])]', '', text2)
+
+        text2=re.sub(r'[\[(]נוסח [^\])]*[\])]', '', text2)
+        text2 = re.sub(r'[^\(\)[\]\{\}a-zA-Zא-ת0-9]*[ה]?תש([\'"״”]+|.[\'"״”]+)[^\(\)]+\d+[^\(\)[\]\{\}a-zA-Zא-ת0-9]*$', '', text2)
+
+        if text != text2 :
+            cleaned_system_rules.append(text2.strip())
+            rules_variations.append({
+                'textOrg': textOrg,
+                'text': text,
+                'text2': text2
+            })
+
+    
+    if rules_variations:
+        # Save cleaned_system_rules to Excel in same directory as this py file
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        out_path = os.path.join(script_dir, "systemRulesWithSpecialSuffix.xlsx")
+        # Save rules_variations (with columns textOrg, text, text2) to Excel
+        df_rules_variations = pd.DataFrame(rules_variations, columns=['textOrg', 'text', 'text2'])
+        df_rules_variations.to_excel(out_path, index=False)
+    
+    
     # Find rules in system_rules that are not in law_texts
     existing_rules = []
         
     for i,viki_rule in enumerate(cleaned_law_texts):
-        if "קרובי משפחה" in viki_rule: #for debugging
+        if "פיצוי וסיוע בשל ביטול טיסה" in viki_rule: #for debugging
             m=1
         for j,system_rule in enumerate(cleaned_system_rules):
-            if "קרובי משפחה" in system_rule: #for debugging
+            if "פיצוי וסיוע בשל ביטול טיסה" in system_rule: #for debugging
                 m=2
 
-            distance = Levenshtein.distance(re.sub(r'[^a-zA-Zא-ת0-9]', '', system_rule.split("*^*")[1]), re.sub(r'[^a-zA-Zא-ת0-9]', '', viki_rule))
-            if (distance < 3 and len(system_rule.split("*^*")[1]) > 20) or (distance < 2 and len(system_rule.split("*^*")[1]) > 5 ):  # Only consider strings longer than 5 characters
-                rule_c_and_name = system_rule.split("*^*")[0]+"*&*"+viki_rule
-                existing_rules.append(rule_c_and_name)
-                break
+            if "*^*" in system_rule:
+                distance = Levenshtein.distance(re.sub(r'[^a-zA-Zא-ת0-9]', '', system_rule.split("*^*")[1]), re.sub(r'[^a-zA-Zא-ת0-9]', '', viki_rule))
+                if (distance < 3 and len(system_rule.split("*^*")[1]) > 20) or (distance < 2 and len(system_rule.split("*^*")[1]) > 5 ):  # Only consider strings longer than 5 characters
+                    rule_c_and_name = system_rule.split("*^*")[0]+"*&*"+viki_rule
+                    existing_rules.append(rule_c_and_name)
+                    break
     
     # Create DataFrame with existing rules
 
@@ -73,7 +104,7 @@ def main():
     df = pd.DataFrame(split_rules, columns=['Rule Index', 'Rule Name'])
     
     # Save to Excel file
-    df.to_excel('existing_rules_111225.xlsx', index=False)
+    df.to_excel('existing_rules_181225.xlsx', index=False)
 
     print (f"✓ finished")
 
